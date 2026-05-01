d60212da54bfbfc3165f65f7cc68297ae71b4b1a
Hernan Robledo <rbldhernan@gmail.com>
2026-05-01 11:32:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-document.querySelectorAll("a").forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-        if (this.getAttribute("href").startsWith("#")) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute("href")).scrollIntoView({
-                behavior: "smooth"
-            });
+// SCROLL SUAVE
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// NAVBAR SCROLL
+window.addEventListener("scroll", () => {
+    document.querySelector(".navbar")
+    .classList.toggle("scrolled", window.scrollY > 50);
+});
+
+// ANIMACIONES
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
         }
     });
 });
+
+document.querySelectorAll(".hidden").forEach(el => observer.observe(el));
